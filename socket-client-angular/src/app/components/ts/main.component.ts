d39af64158b980fd0d7c98bcbac3bfd003ece9ae
francosmp <stha-182@hotmail.com>
2020-01-29 23:16:56 -0500

    2020/01/29 23:16

---
+++ socket-client-angular/src/app/components/ts/main.component.ts
@@ -0,0 +1,76 @@
+import { Component } from '@angular/core';
+import socketIOClient from "socket.io-client";
+
+@Component({
+  selector: 'main',
+  templateUrl: '../html/main.component.html',
+  styleUrls: ['../css/main.component.css']
+})
+
+export class MainComponent {
+
+  response = false
+  endpoint = "http://127.0.0.1:8000" // puede reemplazarse
+  counter = 1
+  username = ''
+  socket = socketIOClient('http://127.0.0.1:8000')
+  messages = []
+  chatPage = false
+  @ViewChild('div') div: ElementRef;
+
+  ngOnInit() {
+    console.log('Init Main')
+
+    // Whenever the server emits 'login', log the login message
+    this.socket.on('login', (data) => {
+      // Display the welcome message
+      data.type = 'login';
+      this.messages = this.messages.concat(data)
+      console.log(this.messages)
+    });
+
+    // Whenever the server emits 'new message', update the chat body
+    this.socket.on('new message', (data) => {
+      data.type = 'message';
+      this.messages = this.messages.concat(data)
+      this.addMessage()
+    });
+    // Whenever the server emits 'user joined', log it in the chat body
+    this.socket.on('user joined', (data) => {
+      data.type = 'log';
+      data.status = 'joined'
+      this.messages = this.messages.concat(data)
+      console.log(this.messages)
+    });
+    // Whenever the server emits 'user left', log it in the chat body
+    this.socket.on('user left', (data) => {
+      data.type = 'log';
+      data.status = 'left'
+      this.messages = this.messages.concat(data)
+      console.log(this.messages)
+    });
+
+  }
+
+  setUsername(e) {
+    this.username = e.target.value
+    this.socket.emit('add user', this.username)
+    this.chatPage = true
+  }
+
+  sendMessage(e) {
+    const data = {
+      username: this.username,
+      message: e.target.value,
+      type: 'message'
+    }
+    this.socket.emit('new message', e.target.value);
+    this.messages = this.messages.concat(data)
+    e.target.value = ''
+    this.addMessage()
+  }
+  addMessage() {
+    console.log(this.messages)
+  }
+
+}
